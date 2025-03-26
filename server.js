b857c1cde5f0ce3747a8dbde5d8ef2d9987d8dd4
Billiefi <2141720051@student.polinema.ac.id>
2025-03-26 11:33:28 +0700

    server

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000;
 const server = app.listen(PORT, async () => {
   // Connect to database
   await connectDB();
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server started on port ${PORT}`);
 });
 
 module.exports = {server};
